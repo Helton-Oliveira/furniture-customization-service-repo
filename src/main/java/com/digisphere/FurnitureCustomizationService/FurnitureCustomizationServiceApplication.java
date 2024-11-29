18f165a178b939b196f2e0855d6e9c01628456e3
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-11-29 19:37:23 -0300

    update

---
+++ src/main/java/com/digisphere/FurnitureCustomizationService/FurnitureCustomizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.digisphere.FurnitureCustomizationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureCustomizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureCustomizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digisphere/FurnitureCustomizationService/FurnitureCustomizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digisphere.FurnitureCustomizationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureCustomizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
