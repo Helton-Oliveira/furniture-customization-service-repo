8fd09915ff00ae112c632278a542276aac167786
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-12-06 12:28:51 -0300

    refactor: refactoring use cases to use a single interface

---
+++ src/main/java/com/digisphere/FurnitureCustomizationService/application/useCase/IUseCase.java
@@ -0,0 +1,7 @@
+package com.digisphere.FurnitureCustomizationService.application.useCase;
+
+import java.util.Map;
+
+public interface IUseCase {
+    String execute(Map<String, String> reqParams);
+}
