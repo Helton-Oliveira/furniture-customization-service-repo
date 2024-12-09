bce34a4f4f1b1f0b5248538a8f064c4cefae7b51
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-12-09 11:57:55 -0300

    feat: implementing test cases for each new command

---
+++ src/test/java/com/digisphere/FurnitureCustomizationService/CommandTest.java
@@ -0,0 +1,120 @@
+package com.digisphere.FurnitureCustomizationService;
+
+import com.digisphere.FurnitureCustomizationService.application.command.IInvoker;
+import com.digisphere.FurnitureCustomizationService.application.command.Invoker;
+import com.digisphere.FurnitureCustomizationService.application.useCase.ProcessOrder;
+import com.digisphere.FurnitureCustomizationService.application.useCase.IProcessOrder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CommandTest {
+
+    @Test
+    @DisplayName("Deve usar o comando para processar pedido de mesa")
+    void createTableCommand() {
+        IProcessOrder processOrder = new ProcessOrder();
+        Map<String, String> data = new HashMap<>();
+        data.put("creatorsId", UUID.randomUUID().toString());
+        data.put("category", "table");
+        data.put("material", "wood");
+        data.put("frameMaterial", "metal");
+        data.put("format", "rectangular");
+        data.put("width", "100cm");
+        data.put("length", "200cm");
+        data.put("height", "150cm");
+        data.put("numberOfFeet", "4");
+        data.put("montage", "detachable");
+        data.put("purpose", "outdoor");
+        data.put("price", "299.90");
+        data.put("quantity", "4");
+        IInvoker invoker = new Invoker(processOrder, data);
+
+        String table = invoker.executeCommand(data.get("category"));
+        assertThat(table).isEqualTo("PEDIDO RELIZADO COM SUCESSO!");
+    }
+
+    @Test
+    @DisplayName("Deve usar o comando para processar pedido de cadeira")
+    void createChairCommand() {
+        IProcessOrder processOrder = new ProcessOrder();
+        Map<String, String> data = new HashMap<>();
+        data.put("creatorsId", UUID.randomUUID().toString());
+        data.put("category", "chair");
+        data.put("quantity", "7");
+        data.put("price", "130.0");
+        data.put("material", "Metal");
+        data.put("upholstery", "Leather");
+        data.put("adjustableHeight", "true");
+        data.put("armrests", "true");
+        data.put("seatRotation", "true");
+        data.put("maximumWeightSupport", "150kg");
+        data.put("legStyle", "Five-star");
+        data.put("seatAndFrameColor", "Black");
+        data.put("purpose", "Office");
+        IInvoker invoker = new Invoker(processOrder, data);
+
+        String chair = invoker.executeCommand(data.get("category"));
+        assertThat(chair).isEqualTo("PEDIDO RELIZADO COM SUCESSO!");
+    }
+
+    @Test
+    @DisplayName("Deve usar o comando para processar pedido de armario")
+    void createCabinetCommand() {
+        IProcessOrder processOrder = new ProcessOrder();
+        Map<String, String> data = new HashMap<>();
+        data.put("creatorsId", UUID.randomUUID().toString());
+        data.put("category", "cabinet");
+        data.put("quantity", "7");
+        data.put("price", "130.0");
+        data.put("material", "Wood");
+        data.put("numberOfDoors", "4");
+        data.put("doorType", "Sliding");
+        data.put("numberOfDrawers", "3");
+        data.put("closingSystem", "Soft-close");
+        data.put("internalDividers", "Yes");
+        data.put("finish", "Matte");
+        data.put("typeOfHandles", "Steel");
+        data.put("internalAndExternalColor", "Brown");
+        data.put("width", "100cm");
+        data.put("height", "200cm");
+        data.put("depth", "50cm");
+        data.put("montage", "Manual");
+        data.put("purpose", "Storage");
+        IInvoker invoker = new Invoker(processOrder, data);
+
+        String cabinet = invoker.executeCommand(data.get("category"));
+        assertThat(cabinet).isEqualTo("PEDIDO RELIZADO COM SUCESSO!");
+    }
+
+    @Test
+    @DisplayName("Deve usar o comando para processar pedido de estante")
+    void createBookCaseCommand() {
+        IProcessOrder processOrder = new ProcessOrder();
+        Map<String, String> data = new HashMap<>();
+        data.put("creatorsId", UUID.randomUUID().toString());
+        data.put("category", "bookCase");
+        data.put("quantity", "7");
+        data.put("price", "130.0");
+        data.put("material", "Wood");
+        data.put("numberOfShelves", "5");
+        data.put("heightBetweenShelves", "30cm");
+        data.put("mouthingSystem", "Wall-Mounted");
+        data.put("width", "100cm");
+        data.put("height", "200cm");
+        data.put("depth", "40cm");
+        data.put("externalFinish", "Glossy");
+        data.put("weightSupportedPerShelf", "20kg");
+        data.put("style", "Modern");
+        data.put("purpose", "recording scenario");
+        IInvoker invoker = new Invoker(processOrder, data);
+
+        String bookCase = invoker.executeCommand(data.get("category"));
+        assertThat(bookCase).isEqualTo("PEDIDO RELIZADO COM SUCESSO!");
+    }
+}
