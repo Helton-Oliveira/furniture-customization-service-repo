01cbd49fffcc1539a823ca02a7eedb4c8346dfce
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-12-09 11:55:49 -0300

    refactor: refactoring field validation

--- src/main/java/com/digisphere/FurnitureCustomizationService/application/utils/RequestValidator.java
+++ src/main/java/com/digisphere/FurnitureCustomizationService/application/utils/RequestValidator.java
@@ -6,13 +6,12 @@ import java.util.Map;
 public class RequestValidator {
 
     public static void validateRequestFields(Map<String, String> fields, List<String> keys) {
+        if (!fields.containsKey("category") || fields.get("category").isBlank()) throw new RuntimeException("ERRO CAMPO (CATEGORIA) NECESSÁRIO PARA GERAR PEDIDO");
         keys.forEach((key) -> {
             if (!fields.containsKey(key)) throw new RuntimeException("ERRO! CAMPO " + key + " NECESSÁRIO PARA CONTINUAR");
             if (fields.get(key).isBlank()) throw new RuntimeException("ERRO! CAMPO " + key + " DEVE CONTER UM VALOR");
         });
     }
 
-    public static void validateFurnitureCategory(Map<String, String> req) {
-        if (!req.containsKey("category") || req.get("category").isBlank()) throw new RuntimeException("ERRO CAMPOR (CATEGORIA) NECESSÁRIO PARA GERAR PEDIDO");
-    }
+
 }
